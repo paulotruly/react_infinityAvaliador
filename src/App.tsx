7a884f42e07d77e2aaeb323364dcce089268558b
paulotruly <paulomsmsms@gmail.com>
2025-10-21 11:29:15 -0300

    projeto-base

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen text-white bg-red-500 flex justify-center items-center">
+      <h1 className="text-4xl font-semibold">Tailwind está funcionando!</h1>
+    </div>
+  )
+}
+
+export default App
